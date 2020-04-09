2d07189b500f5d8debcd0a2cab8e95e2a299b388
Sebastian Holmes <skerlec7693@gmail.com>
2020-04-08 18:07:26 -0700

    added STORE and empty functions to js file

---
+++ script.js
@@ -0,0 +1,115 @@
+const STORE = [
+    {
+        question: 'Louisiana was the ____ state to join the U.S.',
+        answers: [
+            '5th',
+            '15th',
+            '18th',
+            '8th'
+        ],
+        correctAnswer:
+            '18th'
+    },
+    {
+        question: 'Louisiana is also known as the _________.',
+        answers: [
+            'Gumbo State',
+            'Pelican State',
+            'Mosquito State',
+            'Boot State'
+        ],
+        correctAnswer:
+            'Pelican State'
+    },
+    {
+        question: 'What is Louisiana\'s state flower?',
+        answers: [
+            'Water Lily',
+            'Red Rose',
+            'Daisy',
+            'Magnolia'
+        ],
+        correctAnswer:
+            'Magnolia'
+    },
+    {
+        question: 'What is Louisiana\'s state capitol?',
+        answers: [
+            'New Orleans',
+            'Abita Springs',
+            'Shreveport',
+            'Baton Rouge'
+        ],
+        correctAnswer: 
+            'Baton Rouge'
+    },
+    {
+        question: 'What is Louisiana\'s state Bird?',
+        answers: [
+            'Mosquito',
+            'Brown Pelican',
+            'Bald Eagle',
+            'Peacock'
+        ],
+        correctAnswer:
+            'Brown Pelican'
+    }
+];
+
+let score = 0;
+let questionNumber = 0;
+
+function generateQuestion() {
+    // after pressing button go to question.
+}
+
+function resetNums() {
+    // resets score and question numbers.
+}
+
+function newScore() {
+    // update score when question is answered.
+}
+
+function newQuestionNum() {
+    // update question number when question is answered.
+}
+
+function startQuiz() {
+    // start quiz when 'let's geaux' is pressed.
+}
+
+function submitAnswer() {
+    // submit answer and trigger correction or next question.
+}
+
+function questionLayout() {
+    // create html layout for questions and answers.
+}
+
+function correctAnswer() {
+    // corect answer message with button to move forward to next question.
+}
+
+function wrongAnswer() {
+    // give correct answer and button to move forward to next question.
+}
+
+function nextQuestion() {
+    // go to next question after being given feedback.
+}
+
+function finalScore() {
+    // after all questions are answered, final score is given.
+}
+
+function restartQuiz() {
+    // rest quiz and start over when 'geaux again' is pressed.
+}
+
+
+function runQuiz() {
+    // run all necesary functions when page loads.
+}
+
+runQuiz();
