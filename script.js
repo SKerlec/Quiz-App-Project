70355742c492a903e1d9a7b99c0d2d79fc135740
Sebastian Holmes <skerlec7693@gmail.com>
2020-04-09 18:34:34 -0700

    modified html and js files

--- script.js
+++ script.js
@@ -60,56 +60,130 @@ let score = 0;
 let questionNumber = 0;
 
 function generateQuestion() {
-    // after pressing button go to question.
+    if (questionNumber < STORE.length) {
+        return questionLayout(questionNumber);
+    } else {
+        $('.questions-page').hide();
+        finalScore();
+        $('.question-num').text(5);
+    }
 }
 
 function resetNums() {
-    // resets score and question numbers.
+    score = 0;
+    questionNumber = 0;
+    $('.score-num').text(0);
+    $('.question-num').text(0);
 }
 
 function newScore() {
-    // update score when question is answered.
+    score++;
+    $('.score-num').text(score);
 }
 
 function newQuestionNum() {
-    // update question number when question is answered.
+    questionNumber++;
+    $('.question-num').text(questionNumber + 1);
 }
 
 function startQuiz() {
-    // start quiz when 'let's geaux' is pressed.
+    $('.alt-box').hide();
+    $('.center-box').on('click', '.start-button', function(event) {
+        $('.start-page').hide();
+        $('.question-num').text(1);
+        $('.questions-page').show();
+        $('.questions-page').prepend(generateQuestion());
+    });
 }
 
 function submitAnswer() {
-    // submit answer and trigger correction or next question.
+    $('.center-box').on('submit', function(event) {
+        event.preventDefault();
+        $('.alt-box').hide();
+        $('.feedback-page').show();
+        let selected = $('input:checked');
+        let answer = selected.val();
+        let correct = STORE[questionNumber].correctAnswer;
+        if (answer === correct) {
+            correctAnswer();
+        } else {
+            wrongAnswer();
+        }
+    });
 }
 
-function questionLayout() {
-    // create html layout for questions and answers.
+function questionLayout(questionIndex) {
+    let formMaker = $(`<form>
+            <fieldset>
+                <legend class="questionText">${STORE[questionIndex].question}</legend>
+            </fieldset>
+        </form>`)
+    let questionSelector = $(formMaker).find('fieldset');
+    STORE[questionIndex].answers.forEach(function(answerValue, answerIndex) {
+            $(`<label class="progress-nums" for="${answerIndex}">
+                <input class="radio" type="radio" id="${answerIndex}" value="${answerValue}" name="answer" required>
+                    <span>${answerValue}</span>
+                </input>
+            </label>`).appendTo(questionSelector);
+    });
+    $(`<button type="submit" class="submit-answer button">Submit Answer</button>`).appendTo(questionSelector);
+    return formMaker;
 }
 
 function correctAnswer() {
-    // corect answer message with button to move forward to next question.
+    $('.feedback-page').html(
+        `<h2>That's Right!</h2>
+        <img src="correct-img.png" class="pics" alt="Happy Mardi-Gras Mask" width="200px">
+        <button type="button" class="next-question button">Next!</button>`
+    );
+    newScore();
 }
 
 function wrongAnswer() {
-    // give correct answer and button to move forward to next question.
+    $('.feedback-page').html(
+        `<h2>...Wrong...</h2>
+        <img src="wrong-img.png" class="pics" alt="Hurrican Shape" width="200px">
+        <p>The correct answer is...</p>
+        <p>${STORE[questionNumber].correctAnswer}</p>
+        <button type="button" class="next-question button">Next!</button>`
+    );
 }
 
 function nextQuestion() {
-    // go to next question after being given feedback.
+    $('.center-box').on('click', '.next-question', function(event) {
+        $('.alt-box').hide();
+        $('.questions-page').show();
+        newQuestionNum();
+        $('.questions-page form').replaceWith(generateQuestion());
+    });
 }
 
 function finalScore() {
-    // after all questions are answered, final score is given.
+    $('.results-page').show();
+    $('.results-page').html(`
+    <h2>You answered <br>
+    ${score}/5 <br>
+    correct!
+    </h2>
+    <button type="submit" class="restart-quiz button">Geaux Again?</button>`
+    );
 }
 
 function restartQuiz() {
-    // rest quiz and start over when 'geaux again' is pressed.
+    $('.center-box').on('click', '.restart-quiz', function(event) {
+        event.preventDefault();
+        $('.alt-box').hide();
+        resetNums();
+        $('.start-page').show();
+    });
 }
 
-
 function runQuiz() {
-    // run all necesary functions when page loads.
+    startQuiz();
+    generateQuestion();
+    submitAnswer();
+    nextQuestion();
+    restartQuiz();
 }
 
 runQuiz();
